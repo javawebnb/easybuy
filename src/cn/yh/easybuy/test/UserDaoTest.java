1e32f166898cd5a80dcb3b28201c5a8897d8f32f
javawebnb <a123456>
2017-10-22 15:32:44 +0800

    项目框架

---
+++ src/cn/yh/easybuy/test/UserDaoTest.java
@@ -0,0 +1,31 @@
+package cn.yh.easybuy.test;
+
+import org.junit.Test;
+
+import cn.yh.easybuy.entity.User;
+/**
+ * UserDao≤‚ ‘¿‡
+ * @author tigerJay
+ *
+ */
+public class UserDaoTest {
+	
+	@Test
+	public void saveUserTest(){
+		User user = new User();
+		user.setUserName("admin");
+		user.setPassword("admin");
+	}
+	
+	public void updateUserTest(){
+		
+	}
+	
+	public void delUserTest(){
+		
+	}
+
+	public void findUserTest(){
+		
+	}
+}
