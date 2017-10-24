168818773964b6065ee3fb26b0ec2ae6520c9173
dmx <timdmx@163.com>
2017-10-24 16:22:37 +0800

    邓明昕提交订单管理页面

--- src/cn/yh/easybuy/dao/impl/OrderDetailDaoImpl.java
+++ src/cn/yh/easybuy/dao/impl/OrderDetailDaoImpl.java
@@ -1,44 +1,44 @@
 package cn.yh.easybuy.dao.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
+
+import cn.yh.easybuy.dao.OrderDao;
 import cn.yh.easybuy.dao.OrderDetailDao;
+import cn.yh.easybuy.entity.Order;
 import cn.yh.easybuy.entity.OrderDetail;
 import cn.yh.easybuy.utils.SqlSessionFactoryUtil;
 
-/**
- * 
- * @author Administrator
- *
- */
 public class OrderDetailDaoImpl implements OrderDetailDao {
 
 	@Override
 	public List<OrderDetail> findOrderDetail() {
 		// TODO Auto-generated method stub
-		List<OrderDetail> list = null;
+		Map map = null;
 		
 		SqlSession session = SqlSessionFactoryUtil.getSqlSession();
 		OrderDetailDao dao = session.getMapper(OrderDetailDao.class);
 		
-		list = dao.findOrderDetail();
+		List<OrderDetail> list = dao.findOrderDetail();
 		
 		session.commit();
 		session.close();
 		
 		return list;
+		
 	}
 
 	@Override
-	public List<OrderDetail> findDetailByid(Integer d_id) {
+	public List<OrderDetail> findDetailByid(Integer id) {
 		// TODO Auto-generated method stub
 		List<OrderDetail> list = null;
 		
 		SqlSession session = SqlSessionFactoryUtil.getSqlSession();
 		OrderDetailDao dao = session.getMapper(OrderDetailDao.class);
 		
-		list = dao.findDetailByid(d_id);
+		list = dao.findDetailByid(id);
 		
 		session.commit();
 		session.close();
